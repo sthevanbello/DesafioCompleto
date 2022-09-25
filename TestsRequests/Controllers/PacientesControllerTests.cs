2f89887a98af9a4bb354cea53cddab09f7fec9a5
sthevanbello <sthevan.alves@gmail.com>
2022-09-25 18:44:41 -0300

    Criado o projeto para testar a API de modo OnLine É necessário inicializar o projeto da API sem Debug para realizar os testes

---
+++ TestsRequests/Controllers/PacientesControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Desafio_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TestsRequests.Utils;
+using Xunit;
+
+namespace TestsRequests.Controllers
+{
+    public class PacientesControllerTests
+    {
+        [Fact]
+        public void TestGetTodosPacientes()
+        {
+
+            var statusCode = 0;
+            var msg = string.Empty;
+            string baseUrlPaciente = "https://localhost:5001/api/pacientes";
+            try
+            {
+                var email = "marge@simpsons.com";
+                var senha = "marge123456";
+                var token = RequestTokenAPI.GetToken(email, senha);
+                var urlParams = "";
+                var http = new HttpClient { BaseAddress = new Uri($"{baseUrlPaciente}") };
+
+                http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                http.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = http.GetAsync(urlParams).Result;
+
+                var retorno = JsonSerializer.Deserialize<List<Paciente>>(response.Content.ReadAsStringAsync().Result, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                statusCode = (int)response.StatusCode;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+                statusCode = 500;
+            }
+
+            // Retorno
+            Assert.Equal(200, statusCode);
+        }
+    }
+}
---
+++ TestsRequests/Utils/RequestTokenAPI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TestsRequests.Utils
+{
+    public static class RequestTokenAPI
+    {
+
+
+        public static string GetToken(string email, string senha)
+        {
+            const string baseUrl = "https://localhost:5001/api/";
+            var json = JsonSerializer.Serialize(new { email, senha });
+
+            var data = Encoding.ASCII.GetBytes(json);
+
+            string path = $"{baseUrl}Logins";
+            HttpWebRequest request = WebRequest.CreateHttp(path);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = data.Length;
+            request.UserAgent = "RequestToken";
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            HttpWebResponse responseToken = (HttpWebResponse)request.GetResponse();
+            var token = new StreamReader(responseToken.GetResponseStream()).ReadToEnd();
+            return token;
+        }
+    }
+}
